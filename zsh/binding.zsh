e9bbc753447bef6bd305f22119917f2afe074b7e
Joni Eskelinen <jonie@colossus.mobie.fi>
2015-12-14 14:02:21 +0200

    Tweak .zshrc

--- zsh/binding.zsh
+++ zsh/binding.zsh
@@ -8,3 +8,11 @@ bindkey -e
 # Alt + left/right
 bindkey "^[[1;3C" forward-word
 bindkey "^[[1;3D" backward-word
+
+# Bash style delete word backwards
+my-backward-delete-word() {
+    local WORDCHARS=${WORDCHARS/\//}
+    zle backward-delete-word
+}
+zle -N my-backward-delete-word
+bindkey '^W' my-backward-delete-word
--- zsh/history.zsh
+++ zsh/history.zsh
@@ -3,7 +3,7 @@
 #
 
 # History options
-setopt histignorealldups sharehistory
+setopt histignorealldups sharehistory histignorespace
 
 # Keep 1000 lines of history within the shell and save it to ~/.zsh_history:
 HISTSIZE=1000
