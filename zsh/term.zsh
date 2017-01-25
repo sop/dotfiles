a49757d0b5d7d8788090ea8f873ffa233070e125
Joni Eskelinen <joni@mobie.fi>
2017-01-25 15:12:13 +0200

    Enable LS_COLORS

--- zsh/term.zsh
+++ zsh/term.zsh
@@ -5,3 +5,7 @@
 # Set tab width to 4, move one line upwards to mitigate newline caused by tabs
 tabs 4 && echo -ne "\033[1A"
 
+# initialize LS_COLORS
+if [[ -f "$DOTFILES/vendor/LS_COLORS/LS_COLORS" ]]; then
+    eval $( dircolors -b "$DOTFILES/vendor/LS_COLORS/LS_COLORS" )
+fi
